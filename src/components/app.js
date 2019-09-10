34f5f49b742d62fd7db07f30970642d1e5abc213
MatthewJRoybal <matthewjroybal@gmail.com>
2019-09-09 23:39:08 -0700

    Updating

--- src/components/app.js
+++ src/components/app.js
@@ -4,11 +4,13 @@
 *********************************/
 
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import RollingEarth from '../styles/video/RollingEarth.mp4';
 import PropertyPackets from '../styles/images/propertypackets-logo.png';
 import RentPlan from '../styles/images/rentplan-logo.png';
 import Sfdem from '../styles/images/sfdem-logo.png';
+import Netflix from '../styles/images/netflix.png';
+import Countdown from '../styles/images/countdown.png';
+import Calculator from '../styles/images/calculator.png';
 
 class App extends Component {
 	constructor() {
@@ -86,6 +88,54 @@ class App extends Component {
 			          </div>
 
               </div>
+            </div>
+          </div>
+
+					<div id="portfolio" className="jumbotron">
+            <div className="container my-5">
+              <h1 className="text-center my-3">Samples</h1>
+
+              <div className="card-deck">
+
+                <div className="card">
+                  <a href="https://matthewjroybal.github.io/netflixmockup/" target="_blank" rel="noopener noreferrer">
+                    <img className="card-img-top" src={Netflix} alt="Netflix Mockup Logo" />
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Front End (React & Redux)</h5>
+                    <p className="card-text text-justify">Netflix Mockup is an example of using React and Redux. An http call is made with a fetch request to simulate pulling json data which then allows the application to display movie titles to the user. A basic example of Redux is evident in the code managing state for React.</p>
+											<a href="https://matthewjroybal.github.io/netflixmockup/" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-space mr-3">Demo</button></a>
+	                    <a href="https://github.com/MatthewJRoybal/netflixmockup" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-space2">GitHub</button></a>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <a href="https://matthewjroybal.github.io/reactcountdownclock/" target="_blank" rel="noopener noreferrer">
+                    <img className="card-img-top" src={Countdown} alt="Countdown clock for NYE" />
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title font-weight-bold">Date & Time (React)</h5>
+                    <p className="card-text text-justify">This New Years Eve Countdown Clock displays exactly that using setInterval. In addition, it was configured for my local Pacific Standard Time (PST). I hope to find more time to develop this further and add more styling to show off front end skills.</p>
+											<a href="https://matthewjroybal.github.io/reactcountdownclock/" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-space mr-3">Demo</button></a>
+	                    <a href="https://github.com/MatthewJRoybal/reactcountdownclock" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-space2">GitHub</button></a>
+                  </div>
+                </div>
+
+                <div className="card">
+                  <a href="https://matthewjroybal.github.io/Mortgage-Calculator/" target="_blank" rel="noopener noreferrer">
+                    <img className="card-img-top" src={Calculator} alt="Mortgage Calculator Logo" />
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title font-weight-bold">jQuery, Math, & Validation</h5>
+                    <p className="card-text text-justify">Definitely one of my older projects before I dived into React. It helped me learn more about programming. This app best demonstrates the use of a mathematical formula using several input values from the form utilizing bootstrap validation and jquery.</p>
+											<a href="https://matthewjroybal.github.io/Mortgage-Caculator/" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-space mr-3">Demo</button></a>
+	                    <a href="https://github.com/MatthewJRoybal/Mortgage-Caculator" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-space2">GitHub</button></a>
+                  </div>
+                </div>
+
+
+              </div>
+
             </div>
           </div>
 
